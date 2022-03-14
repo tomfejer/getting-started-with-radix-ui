e2bbab868594ad426a355335994fc543569a0a21
tomfejer <24651781+tomfejer@users.noreply.github.com>
2022-03-14 14:09:14 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,226 @@
+import React, { useState } from "react";
+
+import "./styles.css";
+
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpIcon
+} from "@radix-ui/react-icons";
+
+import * as Select from "@radix-ui/react-select";
+import * as Label from "@radix-ui/react-label";
+import * as Switch from "@radix-ui/react-switch";
+import * as Checkbox from "@radix-ui/react-checkbox";
+import * as RadioGroup from "@radix-ui/react-radio-group";
+
+const countriesArray = [
+  "France",
+  "United Kingdom",
+  "Spain",
+  "Hungary",
+  "Netherlands",
+  "Italy",
+  "Germany",
+  "Japan",
+  "Denmark"
+];
+
+const fruitsArray = ["Apple", "Orange", "Lemon", "Melon"];
+
+export default function App() {
+  const [selectDemoValue, setSelectDemoValue] = useState(countriesArray[0]);
+  const [switchDemoValue, setSwitchDemoValue] = useState(true);
+  const [checkboxDemoValue, setCheckboxDemoValue] = useState(true);
+  const [radioGroupDemoValue, setRadioGroupDemoValue] = useState("default");
+
+  return (
+    <div className="App">
+      <div className="Sidebar">
+        <h1 className="SidebarH1">Sidebar</h1>
+        <Label.Root
+          htmlFor="sampleSelectMenu"
+          className="Label"
+          id="sampleSelectMenuLabel"
+        >
+          Select menu title
+        </Label.Root>
+        <Select.Root value={selectDemoValue} onValueChange={setSelectDemoValue}>
+          <Select.Trigger className="SelectTrigger" id="sampleSelectMenu">
+            <Select.Value aria-label={selectDemoValue}>
+              {countriesArray[selectDemoValue]}
+            </Select.Value>
+            <Select.Icon>
+              <ChevronDownIcon className="ChevronIcon" />
+            </Select.Icon>
+          </Select.Trigger>
+          <Select.Content className="SelectContent">
+            <Select.ScrollUpButton className="SelectScrollButtonStyles">
+              <ChevronUpIcon />
+            </Select.ScrollUpButton>
+            <Select.Viewport className="SelectViewport">
+              <Select.Group>
+                <Select.Label className="SelectLabel">Countries</Select.Label>
+                {countriesArray.map((address) => (
+                  <Select.Item value={address} className="SelectItem">
+                    <Select.ItemIndicator className="SelectItemIndicator">
+                      <CheckIcon />
+                    </Select.ItemIndicator>
+                    <Select.ItemText>{address}</Select.ItemText>
+                  </Select.Item>
+                ))}
+              </Select.Group>
+
+              <Select.Separator className="SelectSeparator" />
+
+              <Select.Group>
+                <Select.Label className="SelectLabel">Fruits</Select.Label>
+                {fruitsArray.map((address) => (
+                  <Select.Item value={address} className="SelectItem">
+                    <Select.ItemIndicator className="SelectItemIndicator">
+                      <CheckIcon />
+                    </Select.ItemIndicator>
+                    <Select.ItemText>{address}</Select.ItemText>
+                  </Select.Item>
+                ))}
+              </Select.Group>
+            </Select.Viewport>
+            <Select.ScrollDownButton className="SelectScrollButtonStyles">
+              <ChevronDownIcon />
+            </Select.ScrollDownButton>
+          </Select.Content>
+        </Select.Root>
+
+        <hr className="SidebarSeparator" />
+
+        <Label.Root htmlFor="AirplaneModeSwitch" className="Label">
+          Switch title
+        </Label.Root>
+        <div className="SwitchWrapper" id="AirplaneModeSwitch">
+          <Switch.Root
+            className="Switch"
+            id="sampleSwitch"
+            onCheckedChange={setSwitchDemoValue}
+            checked={switchDemoValue}
+          >
+            <Switch.Thumb className="SwitchThumb" />
+          </Switch.Root>
+          <Label.Root htmlFor="sampleSwitch" className="LabelInline">
+            Airplane mode
+          </Label.Root>
+        </div>
+
+        <hr className="SidebarSeparator" />
+
+        <div className="CheckboxWrapper">
+          <Checkbox.Root
+            className="Checkbox"
+            id="sampleCheckbox"
+            checked={checkboxDemoValue}
+            onCheckedChange={setCheckboxDemoValue}
+          >
+            <Checkbox.Indicator className="CheckboxIndicator">
+              <CheckIcon />
+            </Checkbox.Indicator>
+          </Checkbox.Root>
+          <Label.Root className="LabelInline" htmlFor="sampleCheckbox">
+            Accept terms and conditions.
+          </Label.Root>
+        </div>
+
+        <hr className="SidebarSeparator" />
+        <form>
+          <RadioGroup.Root
+            defaultValue="default"
+            aria-label="View density"
+            className="RadioGroup"
+            value={radioGroupDemoValue}
+            onValueChange={setRadioGroupDemoValue}
+          >
+            <div className="RadioGroupItemWrapper">
+              <RadioGroup.Item
+                value="default"
+                id="r1"
+                className="RadioGroupItem"
+              >
+                <RadioGroup.Indicator className="RadioGroupIndicator" />
+                <img
+                  src="https://source.unsplash.com/featured/?default"
+                  alt="default"
+                  style={{
+                    width: 43,
+                    height: 43,
+                    borderRadius: 3,
+                    position: "absolute",
+                    objectFit: "cover"
+                  }}
+                />
+              </RadioGroup.Item>
+              <Label.Root htmlFor="r1" className="RadioLabel">
+                Default
+              </Label.Root>
+            </div>
+            <div className="RadioGroupItemWrapper">
+              <RadioGroup.Item
+                value="comfortable"
+                id="r2"
+                className="RadioGroupItem"
+              >
+                <RadioGroup.Indicator className="RadioGroupIndicator" />
+                <img
+                  src="https://source.unsplash.com/featured/?comfortable"
+                  alt="default"
+                  style={{
+                    width: 43,
+                    height: 43,
+                    borderRadius: 3,
+                    position: "absolute",
+                    objectFit: "cover"
+                  }}
+                />
+              </RadioGroup.Item>
+              <Label.Root htmlFor="r2" className="RadioLabel">
+                Comfortable
+              </Label.Root>
+            </div>
+            <div className="RadioGroupItemWrapper">
+              <RadioGroup.Item
+                value="compact"
+                id="r3"
+                className="RadioGroupItem"
+              >
+                <RadioGroup.Indicator className="RadioGroupIndicator" />
+                <img
+                  src="https://source.unsplash.com/featured/?compact"
+                  alt="default"
+                  style={{
+                    width: 43,
+                    height: 43,
+                    borderRadius: 3,
+                    position: "absolute",
+                    objectFit: "cover"
+                  }}
+                />
+              </RadioGroup.Item>
+              <Label.Root htmlFor="r3" className="RadioLabel">
+                Compact
+              </Label.Root>
+            </div>
+          </RadioGroup.Root>
+        </form>
+      </div>
+      <div className="StateMonitor">
+        <span className="Label Caption">Select menu value:</span>
+        <span className="Label">{selectDemoValue}</span>
+        <span className="Label Caption">Switch value:</span>
+        <span className="Label">{switchDemoValue === true ? "ON" : "OFF"}</span>
+        <span className="Label Caption">Checkbox value:</span>
+        <span className="Label">
+          {checkboxDemoValue === true ? "ON" : "OFF"}
+        </span>
+        <span className="Label Caption">Radio group value:</span>
+        <span className="Label">{radioGroupDemoValue}</span>
+      </div>
+    </div>
+  );
+}
